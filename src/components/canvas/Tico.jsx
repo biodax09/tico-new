80ebf8ca75137fc42769b04fcb371b6d6970a162
Ömer Yanbaş <biodax09@gmail.com>
2023-03-09 21:44:17 +0300

    bug fix

--- src/components/canvas/Tico.jsx
+++ src/components/canvas/Tico.jsx
@@ -12,7 +12,7 @@ const Ticofab = ({ isMobile }) => {
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
-        angle={0.12}
+        angle={.12}
         penumbra={1}
         intensity={1}
         castShadow
