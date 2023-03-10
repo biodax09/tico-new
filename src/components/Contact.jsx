c168d08fc09b586b49462d27c210df57ef801e7a
Ömer Yanbaş <biodax09@gmail.com>
2023-03-10 04:14:56 +0300

    mail icon

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -74,7 +74,14 @@ const Contact = () => {
       >
         <p className={styles.sectionSubText}>Bize Ulaşın</p>
         <h3 className={styles.sectionHeadText}>İletişim.</h3>
-        <p className={styles}><a href="mailto:hello@ticofab.com">hello@ticofab.com</a></p>
+        <p className={styles}>
+          <a href="mailto:hello@ticofab.com">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+</svg>
+
+          hello@ticofab.com
+          </a></p>
 
         <form
           ref={formRef}
